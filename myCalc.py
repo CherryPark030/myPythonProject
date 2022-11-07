5faf6dca56207e70d996d31fad84df45317a6a87
ChaerinPark98 <chaerin230@gmail.com>
2022-11-07 13:20:20 +0900

    Revert "Mycommit3"
    This reverts commit 40451300c1c74839a80166e38b493ea884a76139.

--- myCalc.py
+++ myCalc.py
@@ -5,12 +5,6 @@ def sum(a, b):
 def sub(a, b):
     print(a - b)
 
-def div(a, b):
-    print(a / b)
-
-def mul(a, b):
-    print( a * b)
-
 #전역 변수부
 
 n1, n2= 100, 200
@@ -19,5 +13,3 @@ n1, n2= 100, 200
 
 sum(n1, n2)
 sub(n1, n2)
-div(n1, n2)
-mul(n1, n2)
